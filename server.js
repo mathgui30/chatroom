b56abb26a377a97213e4e7028dc07d26cbb81025
mathgui30 <matheus.gdepaula@gmail.com>
2024-03-11 16:47:09 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path")
+
+const app = express();
+const server = require("http").createServer(app)
+
+const io = require("socket.io")(server)
+
+
+app.use(express.static(path.join(__dirname+"/chatroom")));
+
+io.on("connection", function(socket) {
+    socket.on("newuser", function( username ) {
+        socket.broadcast.emit("update", username + " entrou na conversa")
+    })
+    socket.on("saiuusuario", function( username ) {
+        socket.broadcast.emit("update", username + " saiu da conversa")
+    })
+    socket.on("chat", function( message ) {
+        socket.broadcast.emit("chat", message)
+    })
+})
+
+server.listen(3000)
